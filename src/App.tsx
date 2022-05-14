aaedce5960c132c4a5c2b615b23835c91a782aae
William Huang <hylbegoat@gmail.com>
2022-05-14 23:21:52 +0800

    feat(page-export): add form to handle page export

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,104 @@
-import React, { useRef } from "react";
-import { useAppVisible } from "./utils";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  useAppVisible,
+  exportToPngImgUrl,
+  savePngImgUrl,
+  useEventListener,
+  usePageName,
+} from "./utils";
 
+const HIDE_UI_KEY_LIST = ["Escape"];
 function App() {
-  const innerRef = useRef<HTMLDivElement>(null);
+  const [querySelector, setQuerySelector] = useState("#main-content-container");
+  const [ignoreClassName, setIgnoreClassName] = useState("references");
+  const [imageUrl, setImageUrl] = useState("");
+  const pageName = usePageName();
   const visible = useAppVisible();
+  useEventListener(
+    "keydown",
+    (e: KeyboardEvent) => {
+      if (HIDE_UI_KEY_LIST.indexOf(e.key) !== -1) {
+        window.logseq.hideMainUI();
+      }
+    },
+    document
+  );
+
+  const exportOnce = useCallback(() => {
+    setImageUrl("");
+    exportToPngImgUrl(querySelector, ignoreClassName.split(" ")).then(
+      (url: string) => {
+        setImageUrl(url);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    exportOnce();
+  }, []);
+
   if (visible) {
     return (
-      <main
-        className="backdrop-filter backdrop-blur-md fixed inset-0 flex items-center justify-center"
-        onClick={(e) => {
-          if (!innerRef.current?.contains(e.target as any)) {
-            window.logseq.hideMainUI();
-          }
-        }}
-      >
-        <div ref={innerRef} className="text-size-2em">
-          Welcome to [[Logseq]] Plugins!
+      <main className="backdrop-filter backdrop-blur-md fixed inset-0 flex flex-col items-center justify-center">
+        <div className="text-black bg-white p-30 rounded">
+          <h1 className="text-2xl text-center">
+            Welcome to use [[Logseq Page Export]]
+          </h1>
+          <br />
+          <br />
+          <div className="flex item-center justify-between">
+            <span>ignoreClassName</span>
+            <input
+              type="text"
+              value={ignoreClassName}
+              onChange={(e) => {
+                setIgnoreClassName(e.target.value);
+              }}
+              className="ml-10 border-2 border-slate-100 grow"
+            ></input>
+          </div>
+          <div className="mt-6 flex item-center justify-between">
+            <span>querySelector</span>
+            <input
+              type="text"
+              value={querySelector}
+              onChange={(e) => {
+                setQuerySelector(e.target.value);
+              }}
+              className="ml-10 border-2 border-slate-100 grow"
+            ></input>
+          </div>
+          <div className="min-h-[300px] border-2 border-slate-100 mt-2 flex justify-center content-center">
+            {imageUrl ? (
+              <img
+                src={imageUrl}
+                className="my-0 mx-auto max-h-[300px] overflow-hidden"
+              />
+            ) : (
+              <span className="text-center">Loading...</span>
+            )}
+          </div>
+          <button
+            className="mt-2 px-6 py-2 bg-yellow-500 font-medium text-sm hover:bg-yellow-600 text-yellow-100 rounded"
+            onClick={() => {
+              exportOnce();
+            }}
+          >
+            Preview Page
+          </button>
+          <button
+            className="ml-2 px-6 py-2 bg-blue-500 font-medium text-sm hover:bg-blue-600 text-blue-100 rounded"
+            onClick={() => {
+              savePngImgUrl(imageUrl, pageName);
+            }}
+          >
+            Save To Disk
+          </button>
         </div>
       </main>
     );
   }
+
   return null;
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { KeyboardEventHandler, useState } from "react";
 import { useMountedState } from "react-use";
+import html2canvas from "html2canvas";
 
 export const useAppVisible = () => {
   const [visible, setVisible] = useState(logseq.isMainUIVisible);
@@ -30,4 +31,120 @@ export const useSidebarVisible = () => {
     });
   }, []);
   return visible;
+};
+
+export const usePageName = () => {
+  const [name, setName] = useState("");
+  const isMounted = useMountedState();
+  React.useEffect(() => {
+    if (isMounted()) {
+      setName("123123");
+    }
+  }, []);
+  return name;
+};
+
+/**
+ * https://usehooks.com/useEventListener/
+ */
+export const useEventListener = (
+  eventName: string,
+  handler: Function,
+  element: HTMLElement | Document
+) => {
+  // Create a ref that stores handler
+  const savedHandler = React.useRef<Function>(handler);
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  React.useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+  React.useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+      // Create event listener that calls handler function stored in ref
+      const eventListener = (event: Event) => savedHandler.current(event);
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element] // Re-run if eventName or element changes
+  );
+};
+
+// https://stackoverflow.com/a/61986511/8106429
+const extractParentCss = () => {
+  const allCSS = [...parent.document.styleSheets]
+    .map((styleSheet) => {
+      try {
+        return [...styleSheet.cssRules].map((rule) => rule.cssText).join("");
+      } catch (e) {
+        console.log(
+          "Access to stylesheet %s is denied. Ignoring...",
+          styleSheet.href
+        );
+      }
+    })
+    .filter(Boolean)
+    .join("\n");
+
+  return allCSS;
+};
+
+const injectStylesToCurrentDocument = (
+  cssText: string,
+  targetDom = document
+) => {
+  const newSS = targetDom.createElement("link");
+  newSS.rel = "stylesheet";
+  newSS.href = "data:text/css," + encodeURIComponent(cssText);
+  targetDom.getElementsByTagName("head")[0].appendChild(newSS);
+};
+
+export const exportToPngImgUrl = (
+  querySelector: string,
+  ignoreClassNameList: Array<string>
+): Promise<string> => {
+  const targetDom = parent.document.querySelector(querySelector) as HTMLElement;
+  const bgColor = parent
+    .getComputedStyle(parent.document.body, null)
+    .getPropertyValue("background-color");
+
+  return html2canvas(targetDom, {
+    allowTaint: true,
+    useCORS: true,
+    backgroundColor: bgColor,
+    ignoreElements: (element) => {
+      return ignoreClassNameList.some((i) => {
+        const { className } = element;
+        if (typeof className === "string") {
+          return className.indexOf(i) !== -1;
+        }
+
+        return false;
+      });
+    },
+    onclone: (clonedDocument) => {
+      injectStylesToCurrentDocument(extractParentCss(), clonedDocument);
+      return clonedDocument;
+    },
+  }).then((canvas) => {
+    const imageUrl = canvas.toDataURL("image/png");
+    return imageUrl;
+  });
+};
+
+export const savePngImgUrl = (imageUrl: string, filename: string) => {
+  let a = document.createElement("a");
+  a.href = imageUrl;
+  a.download = filename;
+  a.click();
 };
